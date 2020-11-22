25342883103d5a97f7888288e17264ba9fbcc60e
GoToHero <go2heromad@gmail.com>
2020-11-22 15:31:47 +0600

    Added MovieDetailsActivity.kt.

--- app/src/main/java/ru/go2hero/androidacademy/MainActivity.kt
+++ app/src/main/java/ru/go2hero/androidacademy/MainActivity.kt
@@ -1,11 +1,18 @@
 package ru.go2hero.androidacademy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textButton = findViewById<TextView>(R.id.textButton)
+        textButton.setOnClickListener {
+            startActivity(Intent(this, MovieDetailsActivity::class.java))
+        }
     }
 }
